6f8242bc06f27de19daf24c4b7281739360fe9ff
Gode King <GengGode@outlook.com>
2022-02-06 14:30:33 +0800

    添加版本控制和资源文件

---
+++ cvAutoTrack/BaiLan/resource/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 cvAutoTrack.rc 使用
+//
+#define IDB_BITMAP_STAR                 101
+#define IDB_BITMAP_PAIMON               102
+#define IDB_BITMAP_UID_                 104
+#define IDB_BITMAP_UID0                 105
+#define IDB_BITMAP_UID1                 106
+#define IDB_BITMAP_UID2                 107
+#define IDB_BITMAP_UID3                 108
+#define IDB_BITMAP_UID4                 109
+#define IDB_BITMAP_UID5                 110
+#define IDB_BITMAP_UID6                 111
+#define IDB_BITMAP_UID7                 112
+#define IDB_BITMAP_UID8                 113
+#define IDB_BITMAP_UID9                 114
+#define IDB_PNG_GIMAP                   115
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
